ac793f3446b2759a884a61d196b92ea3a16a1674
Vishal Kumar <vk2850162@gmail.com>
2025-01-17 22:53:44 +0530

    user authentication done

--- Uber_Backend/routes/user.routes.js
+++ Uber_Backend/routes/user.routes.js
@@ -22,7 +22,7 @@ router.post('/login', [
 
 router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
 
-// router.get('/logout', authMiddleware.authUser, userController.logoutUser)
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 
 
